bb4f179766cdc679381080721df493a93abbe7b8
Bielis08 <bielmonteroroman08@gmail.com>
2024-07-27 14:53:56 +0200

    Calculadora

---
+++ calculadoraGUI.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+#Creacion de GUI
+app = tk.Tk()
+
+#Configuracion principal
+app.geometry("400x450")
+app.configure(background="black")
+app.wm_title('Calculadora')
+app.mainloop()
+
+#Botones
