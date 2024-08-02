efb2346a39718f79ebca7b8d35198a0a4d0be2b4
Bielis08 <bielmonteroroman08@gmail.com>
2024-08-02 20:36:03 +0200

    Problem42

--- 2024/8-Problem 42.py
+++ 2024/8-Problem 42.py
@@ -2,14 +2,12 @@ mensaje = input()
 contador = 0
 lista = mensaje.split()
 encontrado = -1
+signos = {'.', ',', ':', ';', '!', '¡', '?', '¿', '-',}
 for x in lista:
-    puntos = 0
-    encontrado = -1
-    encontrado = x.find('42')
-    if encontrado != -1:
-        for i in x:
-            if i != '0' or '.' or ',':
-                puntos += 1
-        if puntos == 0:
-            contador += 1
+    palabra = ""
+    for i in x:
+        if not i in signos:
+            palabra += i
+    if palabra.find('42') == 0:
+        contador += 1
 print(contador)
